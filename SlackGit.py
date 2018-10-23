e9aa46ad6abe1ba4f7c519ff7442ecba1d18b294
shuro <norish20002000@gmail.com>
2018-10-23 17:20:44 +0900

    first app commit

---
+++ SlackGit.py
@@ -0,0 +1,66 @@
+#!/usr/local/pyenv/shims/python
+# coding:utf-8
+
+import os
+from slackclient import SlackClient
+import AppConf
+import subprocess
+
+class SlackGit:
+    botIdStr = '<@' + AppConf.BOT_ID + '>'
+    sc = SlackClient(AppConf.token)
+    
+    def __init__(self):
+        if SlackGit.sc.rtm_connect():
+            # print(SlackGit.sc.user)
+            # print(SlackGit.sc.server.login_data['self']['id'])
+            while True:
+                data = SlackGit.sc.rtm_read()
+
+                if(len(data) > 0) :
+                    # print(data)
+                    message, channel = self.slackOutputStr(data)
+
+                    print(message)
+                    print(channel)
+
+                    if message and channel:
+                        self.executeCommand(message, channel)
+
+    def slackOutputStr(self, message):
+        messageList = message
+
+        if messageList and len(messageList) > 0:
+            for msg in messageList:
+                if msg and 'text' in msg and SlackGit.botIdStr in msg['text']:
+                    return msg['text'].split(SlackGit.botIdStr)[1].strip().lower(), msg['channel']
+        
+        return None, None
+
+    def executeCommand(self, message, channel):
+        if message == "deploy stg":
+            self.execDeploySh(channel)
+        # elif:
+
+
+            
+    def execDeploySh(self, channel):
+        SlackGit.sc.rtm_send_message(channel, "ステージングサーバーにdeploy、始めます。")
+        try:
+            SlackGit.sc.rtm_send_message(channel, "waitiing")
+            result = subprocess.run(os.path.dirname(__file__) + "/execDeploy.sh", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=True)
+            SlackGit.sc.rtm_send_message(channel, result.stdout.decode('euc_jp'))
+            SlackGit.sc.rtm_send_message(channel, result.stderr.decode('euc_jp'))
+            SlackGit.sc.rtm_send_message(channel, "ステージングサーバーにdeploy、終了しました。")
+    
+            return result
+        except subprocess.CalledProcessError as err:
+            # SlackGit.sc.rtm_send_message(channel, err)
+            print("ERROR:", err)
+            SlackGit.sc.rtm_send_message(channel, "ステージングサーバーにdeploy、失敗しました。\n"\
+                                                    "エラーを確認して下さい。\n" \
+                                                    + err.stderr.decode('euc_jp'))
+
+    
+
+cmd = SlackGit()
